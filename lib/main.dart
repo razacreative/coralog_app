2ee5cad46709643fe3a57cee2ff1d1290b61102a
Azhari Pedia <azharipedia@gmail.com>
2021-05-22 20:06:21 +0530

    Day 5 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:flutter_catalog/utills/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,13 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/Home',
+     // git push --set-upstream origin day4
+   //   initialRoute: '/Home',
       routes: {
-        '/': (context) => LoginPage(),
-        '/Home': (context) => HomePage(),
-        '/Login': (context) => LoginPage(),
+         '/': (context) => LoginPage(),
+         MyRoutes.loginRoutes : (context) => LoginPage(),
+         MyRoutes.homeRoutes: (context) => HomePage(),
       },
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       themeMode: ThemeMode.light,
       darkTheme: ThemeData(
         brightness: Brightness.light,
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utills/routes.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
@@ -10,12 +11,56 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Login Page')),
-      ),
-      body: Container(
-        child: Center(child: Text('Login Loge'),),
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            Image.asset(
+              'assets/login_two.png',
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter Username', labelText: 'Username'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Enter Password', labelText: 'Password'),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoutes);
+                    },
+                    child: Text(
+                      'Login',
+                      style: TextStyle(),
+                    ),
+                    style: TextButton.styleFrom(
+                        minimumSize: Size(100, 40),
+                        backgroundColor: Colors.deepPurple),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

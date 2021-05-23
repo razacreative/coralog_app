325f974ad85053bdb73898f025c9530cb66e434a
Azhari Pedia <azharipedia@gmail.com>
2021-05-23 19:31:36 +0530

    Day 10 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utills/my_routes.dart';
 import 'package:flutter_catalog/widgets/themes.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
